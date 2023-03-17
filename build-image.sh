94033ac77ea73e2bdd17db0f4b625d2aa94dd93c
aasim <h.asim599@gmail.com>
2023-03-17 14:50:26 +0000

    update

--- build-image.sh
+++ build-image.sh
@@ -4,16 +4,16 @@
 IMAGE_NAME="mynginx"
 IMAGE_TAG="$(date +%H-%M)"
 NEW_TAG="latest"
-
+cd /root/nginx-image/
 # git pull repo
 git pull origin master
 
 # Build Docker image
-docker build -f /root/nginx-image/Dockerfile -t $IMAGE_NAME:$IMAGE_TAG 
+docker build . -t $IMAGE_NAME:$IMAGE_TAG 
 
 # Retag Docker image
 docker image tag $IMAGE_NAME:$IMAGE_TAG localhost:5000/$IMAGE_NAME:$NEW_TAG
 
 # Restart Docker Compose service
-docker-compose down && docker compose up 
+docker compose down && docker compose up 
 
